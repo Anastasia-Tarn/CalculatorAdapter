41c323bcc03c58ec5c8f31868e88b019205ce9f3
Anastasiia Tarnovskaia <tarnovskaya-n@yandex.ru>
2022-12-22 18:46:01 +0300

    Initial Commit

---
+++ src/main/java/org/example/IntsCalculator.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class IntsCalculator implements Ints{
+        protected final Calculator target;
+
+        public IntsCalculator() {
+            this.target = new Calculator(); }
+
+        @Override
+        public int sum(int arg0, int arg1) {
+                Calculator.Formula formula = target.newFormula();
+                double result = formula.addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result;
+                return (int) result;
+        }
+
+        @Override
+        public int mult(int arg0, int arg1) {
+                Calculator.Formula formula = target.newFormula();
+                double result = formula.addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT).result;
+                return (int) result;
+        }
+
+        @Override
+        public int pow(int a, int b) {
+                Calculator.Formula formula = target.newFormula();
+                double result = formula.addOperand(a).addOperand(b).calculate(Calculator.Operation.POW).result;
+                return (int) result;
+        }
+}
